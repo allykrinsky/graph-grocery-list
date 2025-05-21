0c1ebcb2a5bd2cb63fbdb4e247b382751e8fdaf7
Allison Krinsky <allisonkrinsky@Allisons-MacBook-Pro.local>
2025-05-21 18:21:01 -0400

    parse notion response into df

--- notion.py
+++ notion.py
@@ -17,26 +17,48 @@ notion = NotionClient(auth=NOTION_TOKEN)
 def query_notion(db_id):
     return notion.databases.query(
         database_id=db_id,
-        filter={
-            "and":[
-                {
-                    "property": "Status",
-                    "status": {
-                        "equals": "Posted"
-                    }},
-                {
-                    "property": "KB",
-                    "checkbox" : {
-                        "equals" : True
-                    }}]  
-        })
+        # filter={
+        #     "and":[
+        #         {
+        #             "property": "Status",
+        #             "status": {
+        #                 "equals": "Posted"
+        #             }},
+        #         {
+        #             "property": "KB",
+        #             "checkbox" : {
+        #                 "equals" : True
+        #             }}]  
+        # }
+    )
 
-# normalize names from notion + convert to DF
-def create_df_from_notion(notion_data):
-    df = pd.DataFrame(notion_data)
-    df['id'] = df['name'].apply(lambda x: normalize_name(x))
+# return DF with columns id, ingredient_name, aisle, recipe_ids
+def parse_ingredient_response(notion_data):
+    return pd.DataFrame([
+        {
+            "id": val["id"],
+            "ingredient_name": val["properties"]["Name"]["title"][0]["plain_text"]
+                if val["properties"]["Name"]["title"] else None,
+            "aisle": val["properties"]["Aisle"]["select"]["name"]
+                if val["properties"]["Aisle"]["select"] else None,
+            "recipe_ids": [r["id"] for r in val["properties"]["Recipes"]["relation"]],
+        }
+        for val in notion_data["results"]
+    ])
 
-    return df
+def create_relationship_table(data):
+    return data.explode('recipe_ids')
 
+# return DF with columns id, recipe_name, type
+def parse_recipe_response(notion_data):
 
-
+    return pd.DataFrame([
+        {
+            "id": val["id"],
+            "recipe_name": val["properties"]["Name"]["title"][0]["plain_text"]
+                if val["properties"]["Name"]["title"] else None,
+            "type": val["properties"]["Type"]["select"]["name"]
+                if val["properties"]["Type"]["select"] else None,
+        }
+        for val in notion_data["results"]
+    ])
